21fad6e0093aadb5ef7ef1daa119ff341a38dd83
moderato <zhongyi.lin.926@gmail.com>
2022-10-30 14:44:27 -0700

    Fixed bw utils and improved accuracy for a2a.

--- analysis/memory_bw_utils.py
+++ analysis/memory_bw_utils.py
@@ -15,53 +15,51 @@ MUL_FACTOR_FUNCS = {
 
 
 # Get turning points of the bus BW curve for a collective
-def get_turning_points(bw_data, ratio_th=0.05):
-    num_samples = len(bw_data)
-    max_bw = bw_data.max()
-    ratios = []
+def get_turning_points(data, ratio_th=0.05, bw='bus_bw', latency='latency'):
+    num_samples = len(data)
+    max_bw = data[bw].max()
+    bw_ratios = []
+    latency_ratios = []
     for idx in range(num_samples):
         if idx == 0 or idx == num_samples - 1:
-            ratios.append(-1)
+            bw_ratios.append(-1)
+            latency_ratios.append(-1)
             continue
-        bw = bw_data[idx]
-        prev_bw = bw_data[idx-1]
-        next_bw = bw_data[idx+1]
-        ratios.append(next_bw * prev_bw / bw / bw)
+        bw_ratios.append(data[bw][idx-1] * data[bw][idx+1] / data[bw][idx] / data[bw][idx])
+        latency_ratios.append(data[latency][idx-1] * data[latency][idx+1] / data[latency][idx] / data[latency][idx])
 
     # Mark the saturation point with 95% of peak
     for idx in reversed(range(num_samples)):
         if idx == 0 or idx == num_samples - 1:
             continue
-        if bw_data[idx] < (1 - ratio_th) * max_bw:
+        if data[bw][idx] < (1 - ratio_th) * max_bw:
             saturation_idx = idx
             break
 
-    # Mark the increment point with 3 consecutive flat samples
-    count = 0
+    # Mark the first jump of latency as increment_idx
+    increment_idx = 4
     for idx in range(num_samples):
-        if idx == 0 or idx == num_samples - 1:
+        if idx < 5 or idx == num_samples - 1:
             continue
-        if abs(ratios[idx] - 1) <= ratio_th:
-            count += 1
-            if count >= 3:
-                increment_idx = idx
-                break
-        else:
-            count = 0
+        if abs(latency_ratios[idx] - 1) > ratio_th * 2:
+            increment_idx = idx - 1
+            break
 
     return increment_idx, saturation_idx
 
 
-def get_feature(collective_data, size='size', bw='bus_bw', latency='latency', all_features=False):
-    incr_idx, sats_idx = get_turning_points(collective_data[bw])
-    incr_p, sats_p = int(np.log2(collective_data[size][incr_idx])), int(np.log2(collective_data[size][sats_idx])) # log sizes of both incr point and sats point
+def get_memory_characteristics(collective_data, size='size', bw='bus_bw', latency='latency'):
+    ln_idx, sats_idx = get_turning_points(collective_data, bw=bw, latency=latency)
+    ln_p, sats_p = int(np.log2(collective_data[size][ln_idx])), int(np.log2(collective_data[size][sats_idx])) # log sizes of both ln point and sats point
     max_bus_bw = collective_data[bw].max() # Pick the bw for the 3rd section of the curves
-    min_bus_bw = collective_data[bw][incr_idx] # Pick the bw for the 1st section of the curves
-    slope = (np.log10(max_bus_bw / min_bus_bw)) / (sats_p - incr_p)
-    overhead = collective_data[latency][0]
-    if all_features:
-        return (incr_p, sats_p, slope, min_bus_bw, max_bus_bw, overhead)
-    return (incr_p, sats_p, max_bus_bw, overhead)
+    overhead = collective_data[latency][max(ln_idx - 1, 0)]
+
+    return {
+        "ln_p": ln_p,
+        "sats_p": sats_p,
+        "max_bw": max_bus_bw,
+        "overhead": overhead,
+    }
 
 
 def sigmoid(x, L, x0, k, b):
@@ -72,10 +70,12 @@ def sigmoid(x, L, x0, k, b):
 def fit_sigmoid_bw_predictor(collective_data, size='size', bw='bus_bw'):
     xdata = np.log2(collective_data[size])
     ydata = np.log10(collective_data[bw])
-    p0 = [ydata.max(), 
-            np.median(xdata),
-            1,
-            ydata.min()] # this is an mandatory initial guess
+    p0 = [
+        ydata.max(), 
+        np.median(xdata),
+        1,
+        ydata.min()
+    ] # this is an mandatory initial guess
     popt, _ = curve_fit(sigmoid, xdata, ydata, p0, method='dogbox')
     return popt
 
@@ -88,24 +88,24 @@ def get_sigmoid_bw(s, sigmoid_param):
     return 10 ** sigmoid(s, *sigmoid_param)
 
 
-# def predict_linear(size, mul_factor, incr_p, sats_p, slope, min_bw, max_bw, overhead):
-#     log_size = np.log2(size)
-#     if log_size <= incr_p:
-#         return overhead
-#     elif log_size >= sats_p:
-#         return size / max_bw * mul_factor / 1e3 + overhead
-#     else:
-#         bw = get_linear_bw(log_size, incr_p, slope, min_bw)
-#         return size / bw * mul_factor / 1e3 + overhead
-
-
-# Sigmoid
-def predict_data_movement_time(size, mul_factor, incr_p, sats_p, max_bw, overhead, sigmoid_param):
+def predict_bw(size, mul_factor, mem_ch, sigmoid_param):
     log_size = np.log2(size)
-    if log_size <= incr_p:
-        return overhead
-    elif log_size >= sats_p:
-        return size / max_bw * mul_factor / 1e3 + overhead
+    if log_size <= mem_ch["ln_p"]:
+        return size / mem_ch["overhead"] / 1e3 # us -> GB/s
+    elif log_size >= mem_ch["sats_p"]:
+        return mem_ch["max_bw"]
     else:
         bw = get_sigmoid_bw(log_size, sigmoid_param)
-        return size / bw * mul_factor / 1e3
+        return bw / mul_factor
+
+
+# In us
+def predict_data_movement_time(size, mul_factor, mem_ch, sigmoid_param):
+    log_size = np.log2(size)
+    if log_size <= mem_ch["ln_p"]:
+        return mem_ch["overhead"]
+    elif log_size >= mem_ch["sats_p"]:
+        return size / mem_ch["max_bw"] * mul_factor / 1e3 + mem_ch["overhead"] # GB/s -> us
+    else:
+        bw = get_sigmoid_bw(log_size, sigmoid_param)
+        return size / bw * mul_factor / 1e3 # GB/s -> us
